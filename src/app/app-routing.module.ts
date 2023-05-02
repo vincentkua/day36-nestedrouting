5581b78c8bbd94725c745ca060db66dbad32115a
atlaskua <zylyn89@gmail.com>
2023-05-03 00:32:28 +0800

    sample routing nested

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductInformationComponent } from './components/product-information/product-information.component';
+import { ProductSpecComponent } from './components/product-spec/product-spec.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '' , component:ProductComponent},
+  {path: 'product' , component:ProductComponent,
+    children: [
+      {path:'details' , component:ProductDetailsComponent,
+        children:[
+        {path:'info/:pid' , component:ProductInformationComponent},
+        {path:'specs' , component:ProductSpecComponent},
+        {path:'**' , redirectTo:'details', pathMatch:'full'},
+        ]
+      }
+    ]    
+  },{path:'**' , redirectTo:'product', pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductInformationComponent } from './components/product-information/product-information.component';
+import { ProductSpecComponent } from './components/product-spec/product-spec.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductDetailsComponent,
+    ProductInformationComponent,
+    ProductSpecComponent
   ],
   imports: [
     BrowserModule,
